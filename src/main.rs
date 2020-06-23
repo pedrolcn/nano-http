8218d865bec7e82b28739a22ed63624dd904234a
Pedro Nascimento <pedrolcn@gmail.com>
2020-06-23 17:49:35 -0300

    feat: mock simple endpoint

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use structopt::StructOpt;
+use tide::{http::Method, Response, Body, Request};
+use femme;
+
+#[derive(Debug, StructOpt, Clone)]
+#[structopt(name="mock-it")]
+struct Options {
+    /// The URL path to mock
+    #[structopt(long)]
+    path: String,
+
+    /// The HTTP method to mock
+    #[structopt(short, long)]
+    method: Option<Method>,
+
+    /// The response to be sent
+    #[structopt(short, long)]
+    respond_with: String,
+
+    /// The response content-type
+    #[structopt(short="t", long, default_value="application/json")]
+    content_type: String,
+}
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    let args: Options = Options::from_args();
+    femme::start(tide::log::Level::Debug.to_level_filter()).unwrap();
+
+    let mut app = tide::with_state(args.clone());
+    let mut route = app.at(args.path.as_str());
+
+    let handler = |req: Request<Options>| async move {
+        let state = req.state();
+
+        let mut res = Response::new(200);
+        res.set_body(Body::from_string(state.respond_with.clone()));
+        res.set_content_type(tide::http::mime::JSON);
+        Ok(res)
+    };
+
+    match args.method {
+        None => route.all(handler),
+        Some(method) => match method {
+            Method::Get => route.get(handler),
+            Method::Post => route.post(handler),
+            Method::Put => route.put(handler),
+            Method::Patch => route.put(handler),
+            Method::Delete => route.put(handler),
+            _ => panic!(format!("Method not supported {}", method))
+        }
+    };
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
